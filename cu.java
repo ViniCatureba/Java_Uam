97871fa86c95c5610bc9251a17638d11add26d0d
Vinicius Marques Gallichio <83302966+ViniciusGallichio@users.noreply.github.com>
2022-05-11 20:42:39 -0300

    Create cu.java
    Coloquei if e else

--- cu.java
+++ cu.java
@@ -2,5 +2,11 @@
 package foo.bar;
 
 public class cu {
-int oi = 1;
+  int cu = 1;
+  if(cu == 1){
+    System.out.println("Vc da o cu");
+  }
+  else{
+    System.out.println("Vc n da o cu");
+  }
 }
